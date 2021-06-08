31446b0a3d0346d2d6a7925c72d660857add9305
vladocetkovic <cetkovic@uns.ac.rs>
2021-06-08 16:46:53 +0200

    API endpoint promena

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,12 @@
-export const OBRAZOVANJE_URL = 'http://localhost:8084/obrazovanje';
-export const PREDUZECE_URL = 'http://localhost:8084/preduzece';
-export const SEKTOR_URL = 'http://localhost:8084/sektor';
-export const RADNIK_URL = 'http://localhost:8084/radnik';
-export const RADNICI_ZA_SEKTOR_URL ='http://localhost:8084/radniciZaSektor';
+// export const OBRAZOVANJE_URL = 'http://localhost:8084/obrazovanje';
+// export const PREDUZECE_URL = 'http://localhost:8084/preduzece';
+// export const SEKTOR_URL = 'http://localhost:8084/sektor';
+// export const RADNIK_URL = 'http://localhost:8084/radnik';
+// export const RADNICI_ZA_SEKTOR_URL ='http://localhost:8084/radniciZaSektor';
+
+
+export const OBRAZOVANJE_URL = 'http://rva-application.herokuapp.com/obrazovanje';
+export const PREDUZECE_URL = 'http://rva-application.herokuapp.com/preduzece';
+export const SEKTOR_URL = 'http://rva-application.herokuapp.com/sektor';
+export const RADNIK_URL = 'http://rva-application.herokuapp.com/radnik';
+export const RADNICI_ZA_SEKTOR_URL ='http://rva-application.herokuapp.com/radniciZaSektor';
